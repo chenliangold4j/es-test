58f7e6a73f7b234fc146628d7fb76e66a25fa4ef
chenliang <chenliangxingji1@163.com>
2020-07-22 23:23:54 +0800

    fix

--- src/main/java/com/gonghui/estest/service/RedirectQueueConsumer.java
+++ src/main/java/com/gonghui/estest/service/RedirectQueueConsumer.java
@@ -1,26 +1,29 @@
 package com.gonghui.estest.service;
 
 import com.gonghui.estest.config.RabbitDeadLetterConfig;
+import com.rabbitmq.client.Channel;
+import com.sun.org.apache.xpath.internal.operations.String;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-@RabbitListener(queues = RabbitDeadLetterConfig.REDIRECT_QUEUE)
+import java.io.IOException;
+
+
 @Component
 @Slf4j
 public class RedirectQueueConsumer {
 
     /**
      * 重定向队列和死信队列形参一致Integer number
-     * @param number
      */
-    @RabbitHandler
-    public void fromDeadLetter(String number){
-        Integer num = Integer.parseInt(number);
-        System.out.println("RedirectQueueConsumer : "+ number);
-        // 对应的操作
-        int i = num / 1;
+    @RabbitListener(queues = RabbitDeadLetterConfig.REDIRECT_QUEUE)
+    public void fromDeadLetter(Message message, Channel channel) throws IOException {
+        java.lang.String msg = new java.lang.String(message.getBody());
+        System.out.println("RedirectQueueConsumer : "+ msg);
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 
 }
