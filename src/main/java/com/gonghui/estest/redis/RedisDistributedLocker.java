692ca15cf45ac274176f1156b5a52e27399d0e70
chenliang <chenliangxingji1@163.com>
2020-07-30 17:40:02 +0800

    fix

--- src/main/java/com/gonghui/estest/redis/RedisDistributedLocker.java
+++ src/main/java/com/gonghui/estest/redis/RedisDistributedLocker.java
@@ -2,6 +2,7 @@ package com.gonghui.estest.redis;
 
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
