40cf1193e8254e7f6dcc9e3da0920fcabf320797
chenliang <chenliangxingji1@163.com>
2020-08-29 17:26:58 +0800

    fix

--- src/main/java/com/gonghui/estest/controller/TestConfig2Controller.java
+++ src/main/java/com/gonghui/estest/controller/TestConfig2Controller.java
@@ -1,5 +1,6 @@
 package com.gonghui.estest.controller;
 
+import com.gonghui.estest.config.RabbitDeadLetterConfig;
 import com.gonghui.estest.config.RabbitMQConfig2;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,12 @@ public class TestConfig2Controller {
     public String send() {
         rabbitTemplate.convertAndSend(RabbitMQConfig2.ITEM_TOPIC_EXCHANGE, RabbitMQConfig2.ITEM_KEY, "年后");
 
+        return "ok";
+    }
+
+    @GetMapping("/test2")
+    public String send2() {
+        rabbitTemplate.convertAndSend(RabbitDeadLetterConfig.DEAD_LETTER_EXCHANGE, RabbitDeadLetterConfig.DEAD_LETTER_REDIRECT_ROUTING_KEY, "ccc");
         return "ok";
     }
 
